55fe51842338b697e347b4754a313be7b4f327b6
Kelly Born <kellyaborn@gmail.com>
2020-06-16 16:15:33 -0400

    refactor subscriptions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FeatureReviewComponent } from './feature-review/feature-review.component';
-import { SubscriptionComponent } from './subscription/subscription.component';
+import { CategoryReviewComponent } from './category-review/category-review.component';
 
 
 const routes: Routes = [
 
   { path: "home", component: FeatureReviewComponent },
-  { path: "subscriptions", component: SubscriptionComponent },
+  { path: "category-review", component: CategoryReviewComponent },
   { path: "", redirectTo: "/home", pathMatch: "full" },
   { path: "**", redirectTo: "/home", pathMatch: "full" }
 
