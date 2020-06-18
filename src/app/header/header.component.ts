63d685b7bd1fd6c178e6be898fdd721e4bf517ce
jjchase <jjchase12@gmail.com>
2020-06-18 16:58:11 -0400

    basic styles

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -18,3 +19,5 @@ export class HeaderComponent implements OnInit {
   }
 
 }
+
+
